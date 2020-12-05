2036a9dae5defbb7a4ce53767df82043ef3abbc4
SebaKrk <s.sciuba@gmail.com>
2020-12-05 11:22:20 +0100

    create a main Image View and set constraints

--- StarWars.Protocols/ChooseSideViewController.swift .swift
+++ StarWars.Protocols/ChooseSideViewController.swift .swift
@@ -9,10 +9,31 @@ import UIKit
 
 class ChooseSideViewController: UIViewController {
     
+    var mainView : UIImageView = {
+        var imageView = UIImageView(image: #imageLiteral(resourceName: "starwars1"))
+        imageView.contentMode = .scaleAspectFit
+//        imageView.adjustsImageSizeForAccessibilityContentSizeCategory = false
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        setUpViewConstraint()
+        
+    }
+    
+//    MARK: - SetUpViewConstraint
+    
+    func setUpViewConstraint() {
+        
+        view.addSubview(mainView)
+        mainView.translatesAutoresizingMaskIntoConstraints = false
+        
+        mainView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mainView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+
     }
 }
 
