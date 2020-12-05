011950de29ed48156cfefd409abaa61bb60c7847
SebaKrk <s.sciuba@gmail.com>
2020-12-05 06:54:20 +0100

    Initial Commit

---
+++ StarWars.Protocols/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StarWars.Protocols
+//
+//  Created by Sebastian Sciuba on 05/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
