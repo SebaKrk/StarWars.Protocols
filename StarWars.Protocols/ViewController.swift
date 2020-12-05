e563733a34550401a3bc93eea7eb956e3145ffaf
SebaKrk <s.sciuba@gmail.com>
2020-12-05 07:49:55 +0100

    create a new UIButton  - logoButton and set constraint

--- StarWars.Protocols/ViewController.swift
+++ StarWars.Protocols/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var logoButton : UIButton = {
+        var button = UIButton(type: .system)
+        button.setImage(UIImage(named: "starWarslogo")?.withRenderingMode(.alwaysOriginal), for: .normal)
+        button.contentMode = .scaleAspectFit
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUpViewConstraint()
+        
     }
-
-
+    
+    
+    //    MARK: - SetUpViewConstraint
+    
+    func setUpViewConstraint() {
+        view.backgroundColor = .black
+        
+        view.addSubview(logoButton)
+        logoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        logoButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        logoButton.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        logoButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+    }
 }
 
